154a47fe12e488051f80c07fea80770b57f0d02f
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-09-26 02:46:39 -0700

    on temperature conversion exercise. challenges with create formlua for converting temps

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let fraction = 5 / 9;
+  let celsius = 0;
+  let fahrenheit = 0;
+
+  if (ftemp = fraction * (35) + 32){
+    return celsius;
+  }
+  
+  };
+  
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CTemp) {
+let fraction = 5 / 9;
+
+if (Ctemp = fraction * ())
+
 };
 
 // Do not edit below this line
