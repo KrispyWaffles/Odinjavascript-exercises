8948f36ce03299b8f460b435c196cf2238484e7c
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-09-25 23:59:35 -0700

    completed removeFromArray test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...itemsToRemove) {
+    return array.filter(item => !itemsToRemove.includes(item));
+}
+    const originalArray = [1, 2, 3, 4, 5];
+    const newArray = removeFromArray(originalArray, 1, "2");
 
+console.log(newArray);
 // Do not edit below this line
 module.exports = removeFromArray;
