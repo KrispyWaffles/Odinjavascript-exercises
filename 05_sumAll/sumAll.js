fc01f169dfc8db79e7043aea2cf11e60dbec20d1
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-09-26 02:01:39 -0700

    completed sumAll testing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+
+let sumOfNumbers = 0;
+
+if (firstNum < 0 || lastNum < 0) {
+    return 'ERROR'
+}
+
+if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+    return 'ERROR';
+}
+
+// if (!isNaN(firstNum) || !isNaN(lastNum)) {
+//     return 'ERROR';
+// }
+
+if (firstNum > lastNum) {
+    [firstNum, lastNum] = [lastNum, firstNum];
+}
+
+for (let i = firstNum; i <= lastNum; i++) {
+    sumOfNumbers +=i;
+}
+
+return sumOfNumbers;   
 
 };
+console.log(sumAll(0, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
