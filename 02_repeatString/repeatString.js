1dcd6e52da96af361febdb0c513624f1a72b4182
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-09-25 23:24:22 -0700

    completed repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+if (num < 0) {
+    return 'ERROR';
+    // return num;
+    } else {
+    return str.repeat(num);
+    }
 };
-
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
